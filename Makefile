afdcdba721d6cb48d9ac767b88ccafd2053bbe45
Federico <federicobrandini@gmail.com>
2024-08-19 21:43:45 +0200

    The weighable interface problem was solved by a concept

--- Makefile
+++ Makefile
@@ -1,17 +1,17 @@
 CC = g++
- 
+CFLAGS = -std=c++20
 OUT = main
  
 all: $(OUT)
  
 main: main.o path.o
-	$(CC) -o $@ $^ 
+	$(CC) $(CFLAGS) -o $@ $^ 
  
 main.o: main.cpp
-	$(CC) -c -o $@ $<
+	$(CC) $(CFLAGS) -c -o $@ $<
  
 path.o: path.cpp path.h
-	$(CC) -c -o $@ $<
+	$(CC) $(CFLAGS) -c -o $@ $<
  
 clean:
 	rm -f *.o *~ $(OUT)
---
+++ weighable.cpp
@@ -0,0 +1,66 @@
+
+#include <concepts>
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+concept Weighable = requires(T a, T b)
+{
+    { a + b } -> std::convertible_to<T>;
+    { a * b } -> std::convertible_to<T>;
+    { T::zero() } -> std::convertible_to<T>;
+    { T::unity() } -> std::convertible_to<T>;
+};
+
+class Distance
+{
+    private:
+        double d;
+
+    public:
+        static Distance zero()
+        {
+            return Distance(0);
+        }
+
+        static Distance unity()
+        {
+            return Distance(1);
+        }
+
+        Distance(double d)
+        {
+            this->d = d;
+        }
+
+        Distance operator + (const Distance& other)
+        {
+            return Distance(d + other.d);
+        }
+
+        Distance operator * (const Distance& other)
+        {
+            return Distance(d * other.d);
+        }
+
+        friend ostream& operator << (ostream& os, const Distance& dis)
+        {
+            os << dis.d;
+            return os;
+        }
+};
+
+template<Weighable T>
+T foo(T a, T b)
+{
+    return T::unity() + T::zero() + a * b;
+}
+
+int main(void)
+{
+    Distance d1(2);
+    Distance d2(8);
+    cout << foo(d1, d2) << endl;
+    return 0;
+}
